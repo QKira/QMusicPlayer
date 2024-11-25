e27ef8973ad3cbd3257d80235c4e6c5653228de2
KiraEx <1976609357@qq.com>
2024-11-25 17:47:58 +0800

    11-25

--- centralwidget.cpp
+++ centralwidget.cpp
@@ -6,9 +6,43 @@ CentralWidget::CentralWidget(QWidget *parent)
     , ui(new Ui::CentralWidget)
 {
     ui->setupUi(this);
+    this->setProperty("form", true);
+
+    QList<QPushButton *> naviBtns = ui->widget_Left->findChildren<QPushButton *>();
+    foreach (QPushButton *naviBtn, naviBtns) {
+        if(naviBtn->objectName() == "btn_Playing") {
+            naviBtn->setChecked(true);
+        }
+        connect(naviBtn, &QPushButton::clicked, this, &CentralWidget::handleNaviButtonClick);
+    }
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 CentralWidget::~CentralWidget()
 {
     delete ui;
+}
+
+void CentralWidget::handleNaviButtonClick()
+{
+    QList<QPushButton *> naviBtns = ui->widget_Left->findChildren<QPushButton *>();
+
+    QPushButton *clickedBtn = (QPushButton *)sender();
+
+    foreach (QPushButton *naviBtn, naviBtns) {
+        if(naviBtn == clickedBtn) {
+            naviBtn->setChecked(true);
+        } else {
+            naviBtn->setChecked(false);
+        }
+    }
+
+    if(clickedBtn->objectName() == "btn_Playing")
+        ui->stackedWidget->setCurrentIndex(0);
+    else if(clickedBtn->objectName() == "btn_Playlists")
+        ui->stackedWidget->setCurrentIndex(1);
+    else if(clickedBtn->objectName() == "btn_Library")
+        ui->stackedWidget->setCurrentIndex(2);
+    else if(clickedBtn->objectName() == "btn_Settings")
+        ui->stackedWidget->setCurrentIndex(3);
 }
--- centralwidget.h
+++ centralwidget.h
@@ -11,12 +11,26 @@ class CentralWidget : public QWidget
 {
     Q_OBJECT
 
+private:
+    enum BTN_NAVI{
+        BTN_NAVI_LIBRARY = 0,
+        BTN_NAVI_PLAYLISTS = 1,
+        BTN_NAVI_PLAYING = 2,
+        BTN_NAVI_SETTINGS = 3,
+    };
+
 public:
     explicit CentralWidget(QWidget *parent = nullptr);
     ~CentralWidget();
 
 private:
     Ui::CentralWidget *ui;
+
+private:
+    void initForm();
+
+private slots:
+    void handleNaviButtonClick();
 };
 
 #endif // CENTRALWIDGET_H
